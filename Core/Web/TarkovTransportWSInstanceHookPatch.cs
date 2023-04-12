23ac733df349f43b40d119da5fb9564f1f1ff0d9
Paulov <pargsoft@hotmail.com>
2023-04-12 20:06:25 +0100

    Hook BSG Request/Server Communication

---
+++ Core/Web/TarkovTransportWSInstanceHookPatch.cs
@@ -0,0 +1,35 @@
+﻿using SIT.Core.Misc;
+using SIT.Tarkov.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIT.Core.Core.Web
+{
+    internal class TarkovTransportWSInstanceHookPatch : ModulePatch
+    {
+        public static TarkovRequestTransportWS TarkovRequestTransportWSInstance { get; set; } = null;
+
+        protected override MethodBase GetTargetMethod()
+        {
+            var t = typeof(TarkovRequestTransportWS);
+
+            return ReflectionHelpers.GetMethodForType(t, "EstablishConnectionToUrl");
+        }
+
+        [PatchPostfix]
+        public static void Postfix(
+            TarkovRequestTransportWS __instance
+            )
+        {
+            if (TarkovRequestTransportWSInstance == null && __instance != null)
+            {
+                TarkovRequestTransportWSInstance = __instance;
+                Logger.LogInfo("Found TarkovRequestTransportWSInstance");
+            }
+        }
+    }
+}
