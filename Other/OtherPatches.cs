9b82a50b536feae969ae806660dfd375785550fd
Paulov <pargsoft@hotmail.com>
2023-04-18 19:08:21 +0100

    Add enable/disable of ThirdParty patches

--- Other/OtherPatches.cs
+++ Other/OtherPatches.cs
@@ -11,6 +11,8 @@ namespace SIT.Core.Other
 
         static AIAwakeOrSleepComponent AIAwakeOrSleepComponent { get; set; }
 
+        static string ConfigSITOtherCategoryValue { get; } = "SIT.Other";
+
         public static void Run(BepInEx.Configuration.ConfigFile config, BaseUnityPlugin plugin)
         {
             m_Config = config;
@@ -18,19 +20,19 @@ namespace SIT.Core.Other
             if (Logger == null)
                 Logger = BepInEx.Logging.Logger.CreateLogSource("Coop");
 
-            var enabled = config.Bind<bool>("Other Patches", "Enable", true);
+            var enabled = config.Bind<bool>(ConfigSITOtherCategoryValue, "Enable", false);
             if (!enabled.Value) // if it is disabled. stop all Other Patches stuff.
             {
                 Logger.LogInfo("Other patches have been disabled! Ignoring Patches.");
                 return;
             }
 
-            if (config.Bind<bool>("Other Patches", "Enable Props AI Bush Patch", true).Value)
+            if (config.Bind<bool>(ConfigSITOtherCategoryValue, "EnablePropsAIBushPatch", false).Value)
                 new AIBushPatch().Enable();
 
-            //var enableAIWakeOrSleep = config.Bind<bool>("Other Patches", "Enable AI Wake or Sleep Patch", true);
-            //if (enableAIWakeOrSleep.Value)
-            //    AIAwakeOrSleepComponent = plugin.GetOrAddComponent<AIAwakeOrSleepComponent>();
+            var enableAIWakeOrSleep = config.Bind<bool>(ConfigSITOtherCategoryValue, "EnableAIWakeOrSleepPatch", false);
+            if (enableAIWakeOrSleep.Value)
+                AIAwakeOrSleepComponent = plugin.GetOrAddComponent<AIAwakeOrSleepComponent>();
         }
     }
 }
