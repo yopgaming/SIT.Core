f3a29f9e3860923ca966fbd5cbf6097ccf43dae4
Paulov <pargsoft@hotmail.com>
2023-04-21 20:48:41 +0100

    Add Looting Replication

--- Coop/Player/FirearmControllerPatches/FirearmController_Loot_Patch.cs
+++ Coop/Player/FirearmControllerPatches/FirearmController_Loot_Patch.cs
@@ -1,77 +1,76 @@
-﻿//using SIT.Coop.Core.Web;
-//using SIT.Core.Misc;
-//using SIT.Tarkov.Core;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Reflection;
+﻿using SIT.Coop.Core.Web;
+using SIT.Core.Misc;
+using SIT.Tarkov.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
-//namespace SIT.Core.Coop.Player.FirearmControllerPatches
-//{
-//    internal class FirearmController_Loot_Patch : ModuleReplicationPatch
-//    {
-//        public override Type InstanceType => typeof(EFT.Player.FirearmController);
-//        public override string MethodName => "Loot";
+namespace SIT.Core.Coop.Player.FirearmControllerPatches
+{
+    internal class FirearmController_Loot_Patch : ModuleReplicationPatch
+    {
+        public override Type InstanceType => typeof(EFT.Player.FirearmController);
+        public override string MethodName => "Loot";
 
-//        protected override MethodBase GetTargetMethod()
-//        {
-//            var method = ReflectionHelpers.GetMethodForType(InstanceType, MethodName);
-//            return method;
-//        }
+        protected override MethodBase GetTargetMethod()
+        {
+            var method = ReflectionHelpers.GetMethodForType(InstanceType, MethodName, findFirst: true);
+            return method;
+        }
 
-//        public static Dictionary<string, bool> CallLocally
-//            = new Dictionary<string, bool>();
+        public static Dictionary<string, bool> CallLocally
+            = new Dictionary<string, bool>();
 
 
-//        [PatchPrefix]
-//        public static bool PrePatch(
-//            EFT.Player.FirearmController __instance
-//            , EFT.Player ____player)
-//        {
-//            var player = ____player;
-//            //var player = ReflectionHelpers.GetAllFieldsForObject(__instance).First(x => x.Name == "_player").GetValue(__instance) as EFT.Player;
-//            if (player == null)
-//                return false;
+        [PatchPrefix]
+        public static bool PrePatch(
+            EFT.Player.FirearmController __instance
+            , EFT.Player ____player)
+        {
+            var player = ____player;
+            if (player == null)
+                return false;
 
-//            var result = false;
-//            if (CallLocally.TryGetValue(player.Profile.AccountId, out var expecting) && expecting)
-//                result = true;
+            var result = false;
+            if (CallLocally.TryGetValue(player.Profile.AccountId, out var expecting) && expecting)
+                result = true;
 
-//            return result;
-//        }
+            return result;
+        }
 
-//        [PatchPostfix]
-//        public static void PostPatch(EFT.Player.FirearmController __instance, ref bool p)
-//        {
-//            var player = ReflectionHelpers.GetAllFieldsForObject(__instance).First(x => x.Name == "_player").GetValue(__instance) as EFT.Player;
-//            if (player == null)
-//                return;
+        [PatchPostfix]
+        public static void PostPatch(EFT.Player.FirearmController __instance, ref bool p)
+        {
+            var player = ReflectionHelpers.GetAllFieldsForObject(__instance).First(x => x.Name == "_player").GetValue(__instance) as EFT.Player;
+            if (player == null)
+                return;
 
-//            if (CallLocally.TryGetValue(player.Profile.AccountId, out var expecting) && expecting)
-//            {
-//                CallLocally.Remove(player.Profile.AccountId);
-//                return;
-//            }
+            if (CallLocally.TryGetValue(player.Profile.AccountId, out var expecting) && expecting)
+            {
+                CallLocally.Remove(player.Profile.AccountId);
+                return;
+            }
 
-//            Dictionary<string, object> dictionary = new Dictionary<string, object>();
-//            dictionary.Add("t", DateTime.Now.Ticks);
-//            dictionary.Add("p", p);
-//            dictionary.Add("m", "Loot");
-//            ServerCommunication.PostLocalPlayerData(player, dictionary);
-//        }
+            Dictionary<string, object> dictionary = new Dictionary<string, object>();
+            dictionary.Add("t", DateTime.Now.Ticks);
+            dictionary.Add("p", p);
+            dictionary.Add("m", "Loot");
+            ServerCommunication.PostLocalPlayerData(player, dictionary);
+        }
 
-//        public override void Replicated(EFT.Player player, Dictionary<string, object> dict)
-//        {
-//            var timestamp = long.Parse(dict["t"].ToString());
-//            if (HasProcessed(GetType(), player, dict))
-//                return;
+        public override void Replicated(EFT.Player player, Dictionary<string, object> dict)
+        {
+            var timestamp = long.Parse(dict["t"].ToString());
+            if (HasProcessed(GetType(), player, dict))
+                return;
 
-//            if (player.HandsController is EFT.Player.FirearmController firearmCont)
-//            {
-//                CallLocally.Add(player.Profile.AccountId, true);
-//                var p = bool.Parse(dict["p"].ToString());
-//                firearmCont.Loot(p);
-//            }
-//        }
-//    }
-//}
+            if (player.HandsController is EFT.Player.FirearmController firearmCont)
+            {
+                CallLocally.Add(player.Profile.AccountId, true);
+                var p = bool.Parse(dict["p"].ToString());
+                firearmCont.Loot(p);
+            }
+        }
+    }
+}
