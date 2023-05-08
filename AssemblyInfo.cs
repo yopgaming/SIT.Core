22925f1e8dd8a184e71227cea6429ded31cef880
Paulov <pargsoft@hotmail.com>
2023-05-08 12:16:58 +0100

    Version Bump

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Windows;
 // Revision
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyVersion("1.2.*")]
 
