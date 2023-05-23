5dd2cff65a3bdb06299d9eb0b87d0d0fcaba219a
Paulov <pargsoft@hotmail.com>
2023-05-23 15:59:51 +0100

    Start class based packet system

---
+++ Coop/NetworkPacket/BasePacket.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIT.Core.Coop.NetworkPacket
+{
+    public abstract class BasePacket
+    {
+        [JsonProperty(PropertyName = "serverId")]
+        public string ServerId { get; set; }
+
+        [JsonProperty(PropertyName = "t")]
+        public float Time { get; set; }
+
+        [JsonProperty(PropertyName = "m")]
+        public virtual string Method { get; set; }
+    }
+}
---
+++ Coop/NetworkPacket/BasePlayerPacket.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SIT.Core.Coop.NetworkPacket
+{
+    public class BasePlayerPacket : BasePacket
+    {
+        [JsonProperty(PropertyName = "accountId")]
+        public string AccountId { get; set; }
+    }
+}
