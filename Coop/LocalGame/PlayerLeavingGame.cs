4eb4483ead9aca4e8f9f7ca2bbc0ce193c510955
Paulov <pargsoft@hotmail.com>
2023-05-27 13:08:10 +0100

    Ensure all Player and Bots "leave" a game at end of match

--- Coop/LocalGame/PlayerLeavingGame.cs
+++ Coop/LocalGame/PlayerLeavingGame.cs
@@ -1,5 +1,7 @@
-﻿using SIT.Core.SP.PlayerPatches;
+﻿using SIT.Coop.Core.Matchmaker;
+using SIT.Core.SP.PlayerPatches;
 using SIT.Tarkov.Core;
+using System.Linq;
 using System.Reflection;
 using UnityEngine;
 
@@ -16,8 +18,35 @@ namespace SIT.Core.Coop.LocalGame
         public static void Postfix(string profileId)
         {
             Logger.LogDebug("PlayerLeavingGame.Postfix");
+
+            
+
             if (CoopGameComponent.TryGetCoopGameComponent(out var component))
             {
+                // Notify that I have left the Server
+                Request.Instance.PostDownWebSocketImmediately(new System.Collections.Generic.Dictionary<string, object>() {
+
+                    { "m", "PlayerLeft" },
+                    { "accountId", component.Players.FirstOrDefault(x=>x.Value.ProfileId == profileId).Value.Profile.AccountId },
+                    { "serverId", CoopGameComponent.GetServerId() }
+
+                });
+
+                // If I am the Host/Server, then ensure all the bots have left too
+                if (MatchmakerAcceptPatches.IsServer)
+                {
+                    foreach(var p in component.Players)
+                    {
+                        Request.Instance.PostDownWebSocketImmediately(new System.Collections.Generic.Dictionary<string, object>() {
+
+                            { "m", "PlayerLeft" },
+                            { "accountId", p.Value.Profile.AccountId },
+                            { "serverId", CoopGameComponent.GetServerId() }
+
+                        });
+                    }
+                }
+
                 Logger.LogDebug("PlayerLeavingGame.Postfix.Destroying CoopGameComponent component");
                 GameObject.Destroy(component);
             }
