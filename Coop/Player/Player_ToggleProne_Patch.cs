dfc92c98a63d6b7ac0ef126b274921fa504328a2
Paulov <pargsoft@hotmail.com>
2023-05-30 08:40:49 +0100

    Removed ToggleProne (i cant trust it)

--- Coop/Player/Player_ToggleProne_Patch.cs
+++ Coop/Player/Player_ToggleProne_Patch.cs
@@ -1,73 +1,73 @@
-﻿using SIT.Coop.Core.Web;
-using SIT.Core.Misc;
-using SIT.Tarkov.Core;
-using System;
-using System.Collections.Generic;
-using System.Reflection;
+﻿//using SIT.Coop.Core.Web;
+//using SIT.Core.Misc;
+//using SIT.Tarkov.Core;
+//using System;
+//using System.Collections.Generic;
+//using System.Reflection;
 
-namespace SIT.Core.Coop.Player
-{
-    public class Player_ToggleProne_Patch : ModuleReplicationPatch
-    {
-        public override Type InstanceType => typeof(EFT.Player);
-        public override string MethodName => "ToggleProne";
+//namespace SIT.Core.Coop.Player
+//{
+//    public class Player_ToggleProne_Patch : ModuleReplicationPatch
+//    {
+//        public override Type InstanceType => typeof(EFT.Player);
+//        public override string MethodName => "ToggleProne";
 
-        protected override MethodBase GetTargetMethod()
-        {
-            var method = ReflectionHelpers.GetMethodForType(InstanceType, MethodName);
+//        protected override MethodBase GetTargetMethod()
+//        {
+//            var method = ReflectionHelpers.GetMethodForType(InstanceType, MethodName);
 
-            return method;
-        }
+//            return method;
+//        }
 
-        public static Dictionary<string, bool> CallLocally
-            = new Dictionary<string, bool>();
+//        public static Dictionary<string, bool> CallLocally
+//            = new Dictionary<string, bool>();
 
-        [PatchPrefix]
-        public static bool PrePatch(EFT.Player __instance)
-        {
-            var result = false;
-            if (CallLocally.TryGetValue(__instance.Profile.AccountId, out var expecting) && expecting)
-                result = true;
+//        [PatchPrefix]
+//        public static bool PrePatch(EFT.Player __instance)
+//        {
+//            var result = false;
+//            if (CallLocally.TryGetValue(__instance.Profile.AccountId, out var expecting) && expecting)
+//                result = true;
 
-            return result;
-        }
+//            return result;
+//        }
 
-        [PatchPostfix]
-        public static void PostPatch(
-           EFT.Player __instance
-            )
-        {
-            var player = __instance;
+//        [PatchPostfix]
+//        public static void PostPatch(
+//           EFT.Player __instance
+//            )
+//        {
+//            var player = __instance;
 
-            if (CallLocally.TryGetValue(player.Profile.AccountId, out var expecting) && expecting)
-            {
-                CallLocally.Remove(player.Profile.AccountId);
-                return;
-            }
+//            if (CallLocally.TryGetValue(player.Profile.AccountId, out var expecting) && expecting)
+//            {
+//                CallLocally.Remove(player.Profile.AccountId);
+//                return;
+//            }
 
-            Dictionary<string, object> dictionary = new Dictionary<string, object>();
-            dictionary.Add("t", DateTime.Now.Ticks);
-            dictionary.Add("m", "ToggleProne");
-            ServerCommunication.PostLocalPlayerData(player, dictionary);
+//            Dictionary<string, object> dictionary = new Dictionary<string, object>();
+//            dictionary.Add("t", DateTime.Now.Ticks);
+//            dictionary.Add("m", "ToggleProne");
+//            ServerCommunication.PostLocalPlayerData(player, dictionary);
 
-        }
+//        }
 
 
-        public override void Replicated(EFT.Player player, Dictionary<string, object> dict)
-        {
-            if (HasProcessed(GetType(), player, dict))
-                return;
+//        public override void Replicated(EFT.Player player, Dictionary<string, object> dict)
+//        {
+//            if (HasProcessed(GetType(), player, dict))
+//                return;
 
-            try
-            {
-                CallLocally.Add(player.Profile.AccountId, true);
-                player.ToggleProne();
-            }
-            catch (Exception e)
-            {
-                Logger.LogError(e);
-            }
-        }
-    }
-}
+//            try
+//            {
+//                CallLocally.Add(player.Profile.AccountId, true);
+//                player.ToggleProne();
+//            }
+//            catch (Exception e)
+//            {
+//                Logger.LogError(e);
+//            }
+//        }
+//    }
+//}
 
