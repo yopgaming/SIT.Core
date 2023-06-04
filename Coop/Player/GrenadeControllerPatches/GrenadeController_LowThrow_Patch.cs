0d088ed94afe16c491d429df12670825417f24d3
Paulov <pargsoft@hotmail.com>
2023-06-04 10:06:30 +0100

    GrenadeController patch stubs

---
+++ Coop/Player/GrenadeControllerPatches/GrenadeController_LowThrow_Patch.cs
@@ -0,0 +1,76 @@
+﻿using SIT.Coop.Core.Web;
+using SIT.Core.Misc;
+using SIT.Tarkov.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIT.Core.Coop.Player.GrenadeControllerPatches
+{
+    internal class GrenadeController_LowThrow_Patch : ModuleReplicationPatch
+    {
+        public override Type InstanceType => typeof(EFT.Player.GrenadeController);
+        public override string MethodName => "LowThrow";
+
+        protected override MethodBase GetTargetMethod()
+        {
+            var method = ReflectionHelpers.GetMethodForType(InstanceType, MethodName, false, true);
+            return method;
+        }
+
+        public static Dictionary<string, bool> CallLocally
+            = new Dictionary<string, bool>();
+
+
+        [PatchPrefix]
+        public static bool PrePatch(
+            EFT.Player.GrenadeController __instance
+            , EFT.Player ____player)
+        {
+            var player = ____player;
+            if (player == null)
+                return false;
+
+            var result = false;
+            if (CallLocally.TryGetValue(player.Profile.AccountId, out var expecting) && expecting)
+                result = true;
+
+            return result;
+        }
+
+        [PatchPostfix]
+        public static void PostPatch(EFT.Player.GrenadeController __instance)
+        {
+            var player = ReflectionHelpers.GetAllFieldsForObject(__instance).First(x => x.Name == "_player").GetValue(__instance) as EFT.Player;
+            if (player == null)
+                return;
+
+            if (CallLocally.TryGetValue(player.Profile.AccountId, out var expecting) && expecting)
+            {
+                CallLocally.Remove(player.Profile.AccountId);
+                return;
+            }
+
+            Dictionary<string, object> dictionary = new Dictionary<string, object>();
+            dictionary.Add("t", DateTime.Now.Ticks);
+            dictionary.Add("m", "LowThrow");
+            ServerCommunication.PostLocalPlayerData(player, dictionary);
+        }
+
+        public override void Replicated(EFT.Player player, Dictionary<string, object> dict)
+        {
+            if (HasProcessed(GetType(), player, dict))
+                return;
+
+            if (player.HandsController is EFT.Player.GrenadeController cont)
+            {
+                Logger.LogDebug("GrenadeController_LowThrow_Patch:Replicated");
+                CallLocally.Add(player.Profile.AccountId, true);
+                cont.LowThrow();
+            }
+        }
+    }
+}
---
+++ Coop/Player/GrenadeControllerPatches/GrenadeController_PullRingForHighThrow_Patch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIT.Core.Coop.Player.GrenadeControllerPatches
+{
+    internal class GrenadeController_PullRingForHighThrow_Patch
+    {
+    }
+}
