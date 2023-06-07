3a51cc7433b4de83a2ad6d753510ab336509054d
Paulov <pargsoft@hotmail.com>
2023-06-07 20:45:21 +0100

    Create ChangeHydration and Energy stubs

---
+++ Coop/Player/Health/ChangeHydrationPatch.cs
@@ -0,0 +1,36 @@
+﻿//using SIT.Core.Misc;
+//using SIT.Tarkov.Core;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Reflection;
+
+//namespace SIT.Core.Coop.Player.Health
+//{
+//    internal class ChangeHydrationPatch : ModuleReplicationPatch
+//    {
+//        public override Type InstanceType => typeof(PlayerHealthController);
+
+//        public override string MethodName => "ChangeHydration";
+
+//        protected override MethodBase GetTargetMethod()
+//        {
+//            return ReflectionHelpers.GetMethodForType(typeof(PlayerHealthController), "ChangeHydration", findFirst: true);
+//        }
+
+//        [PatchPostfix]
+//        public static void PatchPostfix(
+//            PlayerHealthController __instance
+//            , float value
+//            )
+//        {
+//            var player = __instance.Player;
+
+
+//        }
+
+//        public override void Replicated(EFT.Player player, Dictionary<string, object> dict)
+//        {
+//        }
+//    }
+//}
