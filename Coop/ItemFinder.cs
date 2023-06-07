478a83e95ea7a81c3587c41c33060394eeb7f6b1
Paulov <pargsoft@hotmail.com>
2023-06-07 20:12:09 +0100

    Add Helper: Search for item inventory or world

---
+++ Coop/ItemFinder.cs
@@ -0,0 +1,40 @@
+﻿using Comfort.Common;
+using EFT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIT.Core.Coop
+{
+    public static class ItemFinder
+    {
+        public static bool TryFindItemOnPlayer(EFT.Player player, string templateId, string itemId, out EFT.InventoryLogic.Item item)
+        {
+            item = null;
+
+            var allItemsOfTemplate = player.Profile.Inventory.GetAllItemByTemplate(templateId);
+
+            if (!allItemsOfTemplate.Any())
+                return false;
+
+            item = allItemsOfTemplate.FirstOrDefault(x => x.Id == itemId);
+            return item != null;
+        }
+
+        public static bool TryFindItemInWorld(EFT.Player player, string itemId, out EFT.InventoryLogic.Item item)
+        {
+            item = null;
+
+            var itemFindResult = Singleton<GameWorld>.Instance.FindItemById(itemId);
+            if (itemFindResult.Succeeded)
+            {
+                item = itemFindResult.Value;
+            }
+
+            return item != null;
+
+        }
+    }
+}
