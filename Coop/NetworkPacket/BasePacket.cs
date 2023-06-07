c01f55da09027cb2c6dbc6ee8be7cce72ab1daf3
Paulov <pargsoft@hotmail.com>
2023-06-07 20:11:16 +0100

    BasePacket to use ServerId automatically

--- Coop/NetworkPacket/BasePacket.cs
+++ Coop/NetworkPacket/BasePacket.cs
@@ -10,12 +10,12 @@ namespace SIT.Core.Coop.NetworkPacket
     public abstract class BasePacket
     {
         [JsonProperty(PropertyName = "serverId")]
-        public string ServerId { get; set; }
+        public string ServerId { get; set; } = CoopGameComponent.GetServerId();
 
         [JsonProperty(PropertyName = "t")]
-        public float Time { get; set; }
+        public float Time { get; set; } = DateTime.Now.Ticks;
 
         [JsonProperty(PropertyName = "m")]
-        public virtual string Method { get; set; }
+        public virtual string Method { get; set; } = null;
     }
 }
---
+++ Coop/NetworkPacket/HealPacket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIT.Core.Coop.NetworkPacket
+{
+    public class HealPacket : BasePlayerPacket
+    {
+        public EBodyPart bodyPart { get; set; }
+        public float value { get; set; }
+        public override string Method { get => "Heal"; }
+    }
+}
