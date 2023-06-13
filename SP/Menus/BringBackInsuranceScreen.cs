7d6fdce5eedcb72bbe562419075f11708ba5457f
Paulov <pargsoft@hotmail.com>
2023-06-13 19:35:31 +0100

    Fix #147 Partially (Insurance screen was missing)

--- SP/Menus/BringBackInsuranceScreen.cs
+++ SP/Menus/BringBackInsuranceScreen.cs
@@ -22,7 +22,7 @@ namespace SIT.Core.SP.Menus
         protected override MethodBase GetTargetMethod()
         {
             var desiredType = typeof(MainMenuController);
-            var desiredMethod = desiredType.GetMethod("method_62", BindingFlags.NonPublic | BindingFlags.Instance);
+            var desiredMethod = desiredType.GetMethod("method_66", BindingFlags.NonPublic | BindingFlags.Instance);
 
             Logger.LogDebug($"{GetType().Name} Type: {desiredType?.Name}");
             Logger.LogDebug($"{GetType().Name} Method: {desiredMethod?.Name}");
