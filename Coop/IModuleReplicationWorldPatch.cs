856a5452397518f0e55a49110b71c1d554df52c1
Paulov <pargsoft@hotmail.com>
2023-06-15 18:14:51 +0100

    New Interface for Replication of World Packets

---
+++ Coop/IModuleReplicationWorldPatch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SIT.Core.Coop
+{
+    public interface IModuleReplicationWorldPatch
+    {
+        public Type InstanceType { get; }
+        public string MethodName { get; }
+        public bool DisablePatch { get; }
+
+        public void Replicated(Dictionary<string, object> packet);
+
+    }
+}
