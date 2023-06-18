377c754df85126307c338017343efd65a6d9f3ec
Paulov <pargsoft@hotmail.com>
2023-06-18 21:04:07 +0100

    Re-Enabled Pickup patch (test)

--- Coop/Player/FirearmControllerPatches/FirearmController_Pickup_Patch.cs
+++ Coop/Player/FirearmControllerPatches/FirearmController_Pickup_Patch.cs
@@ -1,77 +1,113 @@
-﻿//using SIT.Coop.Core.Web;
-//using SIT.Core.Misc;
-//using SIT.Tarkov.Core;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Reflection;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using SIT.Coop.Core.Web;
+using SIT.Core.Coop.NetworkPacket;
+using SIT.Core.Misc;
+using SIT.Tarkov.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static SIT.Core.Coop.Player.FirearmControllerPatches.FirearmController_SetTriggerPressed_Patch;
 
-//namespace SIT.Core.Coop.Player.FirearmControllerPatches
-//{
-//    internal class FirearmController_Pickup_Patch : ModuleReplicationPatch
-//    {
-//        public override Type InstanceType => typeof(EFT.Player.FirearmController);
-//        public override string MethodName => "FCPickup";
+namespace SIT.Core.Coop.Player.FirearmControllerPatches
+{
+    internal class FirearmController_Pickup_Patch : ModuleReplicationPatch
+    {
+        public override Type InstanceType => typeof(EFT.Player.FirearmController);
+        public override string MethodName => "FCPickup";
 
-//        protected override MethodBase GetTargetMethod()
-//        {
-//            var method = ReflectionHelpers.GetMethodForType(InstanceType, "Pickup", true);
-//            return method;
-//        }
+        protected override MethodBase GetTargetMethod()
+        {
+            var method = ReflectionHelpers.GetMethodForType(InstanceType, "Pickup", false, true);
+            return method;
+        }
 
-//        public static Dictionary<string, bool> CallLocally
-//            = new Dictionary<string, bool>();
+        public static Dictionary<string, bool> CallLocally
+            = new Dictionary<string, bool>();
 
 
-//        [PatchPrefix]
-//        public static bool PrePatch(
-//            EFT.Player.FirearmController __instance
-//            , EFT.Player ____player)
-//        {
-//            var player = ____player;
-//            if (player == null)
-//                return false;
+        [PatchPrefix]
+        public static bool PrePatch(
+            EFT.Player.FirearmController __instance
+            , EFT.Player ____player
+            )
+        {
+            //Logger.LogInfo("FirearmController_Pickup_Patch.PrePatch");
 
-//            var result = false;
-//            if (CallLocally.TryGetValue(player.Profile.AccountId, out var expecting) && expecting)
-//                result = true;
+            if (CoopGameComponent.GetCoopGameComponent() == null)
+                return false;
 
-//            return result;
-//        }
+            if (CoopGameComponent.GetCoopGameComponent().HighPingMode && ____player.IsYourPlayer)
+            {
+                return true;
+            }
 
-//        [PatchPostfix]
-//        public static void PostPatch(EFT.Player.FirearmController __instance, bool p)
-//        {
-//            var player = ReflectionHelpers.GetAllFieldsForObject(__instance).First(x => x.Name == "_player").GetValue(__instance) as EFT.Player;
-//            if (player == null)
-//                return;
+            var player = ____player;
+            if (player == null)
+                return false;
 
-//            if (CallLocally.TryGetValue(player.Profile.AccountId, out var expecting) && expecting)
-//            {
-//                CallLocally.Remove(player.Profile.AccountId);
-//                return;
-//            }
+            var result = false;
+            if (CallLocally.TryGetValue(player.Profile.AccountId, out var expecting) && expecting)
+                result = true;
 
-//            Dictionary<string, object> dictionary = new Dictionary<string, object>();
-//            dictionary.Add("t", DateTime.Now.Ticks);
-//            dictionary.Add("p", p.ToString());
-//            dictionary.Add("m", "FCPickup");
-//            ServerCommunication.PostLocalPlayerData(player, dictionary);
-//        }
+            return result;
+        }
 
-//        public override void Replicated(EFT.Player player, Dictionary<string, object> dict)
-//        {
-//            var timestamp = long.Parse(dict["t"].ToString());
-//            if (HasProcessed(GetType(), player, dict))
-//                return;
+        [PatchPostfix]
+        public static void PostPatch(
+            EFT.Player.FirearmController __instance
+            , EFT.Player ____player
+            , bool p)
+        {
+            var player = ____player;
+            if (CallLocally.TryGetValue(player.Profile.AccountId, out var expecting) && expecting)
+            {
+                CallLocally.Remove(player.Profile.AccountId);
+                return;
+            }
 
-//            if (player.HandsController is EFT.Player.FirearmController firearmCont)
-//            {
-//                CallLocally.Add(player.Profile.AccountId, true);
-//                firearmCont.Pickup(bool.Parse(dict["p"].ToString()));
-//            }
-//        }
-//    }
-//}
+            FCPickupPicket pickupPicket = new FCPickupPicket(player.Profile.AccountId, p);
+            Request.Instance.SendDataToPool(pickupPicket.Serialize());
+        }
+
+        public override void Replicated(EFT.Player player, Dictionary<string, object> dict)
+        {
+            Logger.LogInfo("FirearmController_Pickup_Patch.Replicated");
+
+            if (CoopGameComponent.GetCoopGameComponent().HighPingMode && player.IsYourPlayer)
+            {
+                // You would have already run this. Don't bother
+                return;
+            }
+
+            FCPickupPicket pp = new(null,false);
+
+            if (dict.ContainsKey("data"))
+            {
+                pp = pp.DeserializePacketSIT(dict["data"].ToString());
+            }
+
+            if (HasProcessed(GetType(), player, pp))
+                return;
+
+            if (player.HandsController is EFT.Player.FirearmController firearmCont)
+            {
+                CallLocally.Add(player.Profile.AccountId, true);
+                firearmCont.Pickup(pp.Pickup);
+            }
+        }
+
+        public class FCPickupPicket : BasePlayerPacket
+        {
+            public bool Pickup { get; set; }
+
+            public FCPickupPicket(string accountId, bool pickup)
+            {
+                AccountId = accountId;
+                Pickup = pickup;
+                Method = "FCPickup";
+            }
+        }
+    }
+}
