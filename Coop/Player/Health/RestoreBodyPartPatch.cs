b9ef98fd4dc447084843f9c3a8f31a06649c4ac9
Paulov <pargsoft@hotmail.com>
2023-07-01 17:19:54 +0100

    Fix error occuring on RestoryBodyPartPatch

--- Coop/Player/Health/RestoreBodyPartPatch.cs
+++ Coop/Player/Health/RestoreBodyPartPatch.cs
@@ -93,24 +93,43 @@ namespace SIT.Core.Coop.Player.Health
                     return;
                 }
                 bodyPartDict[restoreBodyPartPacket.BodyPart].IsDestroyed = false;
+                var healthPenalty = restoreBodyPartPacket.HealthPenalty + (1f - restoreBodyPartPacket.HealthPenalty) * (float)player.Skills.SurgeryReducePenalty;
+                Logger.LogDebug("RestoreBodyPart::HealthPenalty::" + healthPenalty);
                 bodyPartDict[restoreBodyPartPacket.BodyPart].Health 
-                    = new HealthValue(1f, Mathf.Max(1f, Mathf.Ceil(bodyPartDict[restoreBodyPartPacket.BodyPart].Health.Maximum * restoreBodyPartPacket.HealthPenalty)), 0f);
+                    = new HealthValue(1f, Mathf.Max(1f, Mathf.Ceil(bodyPartDict[restoreBodyPartPacket.BodyPart].Health.Maximum * healthPenalty)), 0f);
             }
 
 
         }
 
-        private IReadOnlyDictionary<EBodyPart, AHealthController<AbstractHealth2.AbstractHealthEffect2>.BodyPartState> GetBodyPartDictionary(EFT.Player player)
+        private IReadOnlyDictionary<EBodyPart, AHealthController<AbstractHealth.AbstractHealthEffect>.BodyPartState> GetBodyPartDictionary(EFT.Player player)
         {
-            IReadOnlyDictionary<EBodyPart, AHealthController<AbstractHealth2.AbstractHealthEffect2>.BodyPartState> bodyPartDict
-    = ReflectionHelpers.GetFieldOrPropertyFromInstance<IReadOnlyDictionary<EBodyPart, AHealthController<AbstractHealth2.AbstractHealthEffect2>.BodyPartState>>(player.PlayerHealthController, "IReadOnlyDictionary_0", false);
-            if (bodyPartDict == null)
+            try
             {
-                Logger.LogError($"Could not retreive {player.Id}'s Health State Dictionary");
-                return null;
+                var bodyPartDict
+                = ReflectionHelpers.GetFieldOrPropertyFromInstance<IReadOnlyDictionary<EBodyPart, AHealthController<AbstractHealth.AbstractHealthEffect>.BodyPartState>>(player.PlayerHealthController, "IReadOnlyDictionary_0", false);
+                if (bodyPartDict == null)
+                {
+                    Logger.LogError($"Could not retreive {player.Id}'s Health State Dictionary");
+                    return null;
+                }
+                //Logger.LogInfo(bodyPartDict.ToJson());
+                return bodyPartDict;
             }
-            //Logger.LogInfo(bodyPartDict.ToJson());
-            return bodyPartDict;
+            catch (Exception)
+            {
+
+                var field = ReflectionHelpers.GetFieldFromType(player.PlayerHealthController.GetType(), "IReadOnlyDictionary_0");
+                Logger.LogError(field);
+                var type = field.DeclaringType;
+                Logger.LogError(type);
+                var val = field.GetValue(player.PlayerHealthController);
+                Logger.LogError(val);
+                var valType = field.GetValue(player.PlayerHealthController).GetType();
+                Logger.LogError(valType);
+            }
+
+            return null;
         }
 
         public class RestoreBodyPartPacket : BasePlayerPacket
