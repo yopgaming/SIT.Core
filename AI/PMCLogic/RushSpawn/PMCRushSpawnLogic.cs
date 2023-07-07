30e86a544710e3e2b40fcd490a0c328c0f4b81a0
Paulov <pargsoft@hotmail.com>
2023-07-07 23:26:56 +0100

    AI Logic Stubs

---
+++ AI/PMCLogic/RushSpawn/PMCRushSpawnLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIT.Core.AI.PMCLogic.RushSpawn
+{
+    internal class PMCRushSpawnLogic
+    {
+    }
+}
