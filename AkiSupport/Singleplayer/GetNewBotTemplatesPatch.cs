2aee0186ce3e9b85613d0c895d716816cab7edd4
Paulov <pargsoft@hotmail.com>
2023-07-09 19:43:51 +0100

    Use SPT-Aki latest GetNewBotTemplatesPatch

--- AkiSupport/Singleplayer/GetNewBotTemplatesPatch.cs
+++ AkiSupport/Singleplayer/GetNewBotTemplatesPatch.cs
@@ -25,52 +25,15 @@ namespace SIT.Core.AkiSupport.Singleplayer
         [PatchPrefix]
         private static bool PatchPrefix(ref Task<Profile> __result, BotsPresets __instance, CreationData data, bool withDelete)
         {
-            if (PatchConstants.BackEndSession == null)
-            {
-                Logger.LogDebug("GetNewBotTemplatesPatch BackEndSession is NULL?");
-                return true;
-            }
-
-            GetLogger(typeof(GetNewBotTemplatesPatch)).LogDebug("GetNewBotTemplatesPatch.PatchPrefix");
-
-            /*
-                in short when client wants new bot and GetNewProfile() return null (if not more available templates or they don't satisfy by Role and Difficulty condition)
-                then client gets new piece of WaveInfo collection (with Limit = 30 by default) and make request to server
-                but use only first value in response (this creates a lot of garbage and cause freezes)
-                after patch we request only 1 template from server
-
-                along with other patches this one causes to call data.PrepareToLoadBackend(1) gets the result with required role and difficulty:
-                new[] { new WaveInfo() { Limit = 1, Role = role, Difficulty = difficulty } }
-                then perform request to server and get only first value of resulting single element collection
-            */
-
-            var taskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
-            var taskAwaiter = (Task<Profile>)null;
-
-            try
-            {
-                var profile = _getNewProfileMethod.Invoke(__instance, new object[] { data, true });
-            }
-            catch (Exception e)
-            {
-                Logger.LogDebug($"getnewbot failed: {e.Message} {e.InnerException}");
-                throw;
-            }
-
-            // load from server
-            var source = data.PrepareToLoadBackend(1).ToList();
-            taskAwaiter = PatchConstants.BackEndSession.LoadBots(source).ContinueWith(GetFirstResult, taskScheduler);
-
-            // load bundles for bot profile
-            var continuation = new Aki.Custom.Models.BundleLoader(taskScheduler);
-            __result = taskAwaiter.ContinueWith(continuation.LoadBundles, taskScheduler).Unwrap();
-            return false;
+            withDelete = true;
+            return true; // do original method
         }
 
         private static Profile GetFirstResult(Task<Profile[]> task)
         {
             var result = task.Result[0];
-            Logger.LogDebug($"Loading bot profile from server. role: {result.Info.Settings.Role} side: {result.Side}");
+            Logger.LogInfo($"{DateTime.Now:T} Loading bot {result.Info.Nickname} profile from server. role: {result.Info.Settings.Role} side: {result.Side}");
+
             return result;
         }
     }
