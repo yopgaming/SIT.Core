098488909357e84b1f8690cdbc379fc73e1d45d2
Paulov <pargsoft@hotmail.com>
2023-07-21 11:00:31 +0100

    Moving Damage Code to CoopPlayer

--- Coop/Player/Player_ApplyDamageInfo_Patch.cs
+++ Coop/Player/Player_ApplyDamageInfo_Patch.cs
@@ -1,5 +1,6 @@
 ﻿using EFT;
 using EFT.InventoryLogic;
+using SIT.Coop.Core.Matchmaker;
 using SIT.Coop.Core.Player;
 using SIT.Coop.Core.Web;
 using SIT.Core.Configuration;
@@ -15,9 +16,10 @@ namespace SIT.Core.Coop.Player
     public class Player_ApplyDamageInfo_Patch : ModuleReplicationPatch
     {
         //private static ConcurrentDictionary<string, long> ProcessedCalls = new();
-        public static Dictionary<string, bool> CallLocally = new();
+        public static List<string> CallLocally = new();
         public override Type InstanceType => typeof(EFT.Player);
         public override string MethodName => "ApplyDamageInfo";
+
         //public override bool DisablePatch => true;
 
         protected override MethodBase GetTargetMethod()
@@ -33,7 +35,7 @@ namespace SIT.Core.Coop.Player
             , EBodyPart bodyPartType)
         {
             var result = true;
-            if (CallLocally.TryGetValue(__instance.Profile.AccountId, out var expecting) && !expecting)
+            if (!CallLocally.Contains(__instance.ProfileId))
                 result = false;
 
             if (!LastDamageTypes.ContainsKey(__instance.ProfileId))
@@ -71,9 +73,9 @@ namespace SIT.Core.Coop.Player
         {
             var player = __instance;
 
-            if (CallLocally.TryGetValue(player.Profile.AccountId, out var expecting) && expecting)
+            if (CallLocally.Contains(player.ProfileId))
             {
-                CallLocally.Remove(player.Profile.AccountId);
+                CallLocally.Remove(player.ProfileId);
                 return;
             }
 
@@ -92,63 +94,68 @@ namespace SIT.Core.Coop.Player
                 }
             }
 
+            // Test.
+            // Lets see if we can only run this from a server only perspective
+            // 
+            //if (MatchmakerAcceptPatches.IsClient)
+            //    return;
 
-            Dictionary<string, object> packet = new();
-            damageInfo.HitCollider = null;
-            damageInfo.HittedBallisticCollider = null;
-            Dictionary<string, string> playerDict = new();
-            try
-            {
-                if (damageInfo.Player != null)
-                {
-                    //playerDict.Add("d.p.aid", damageInfo.Player.Profile.AccountId);
-                    playerDict.Add("d.p.aid", damageInfo.Player.iPlayer.Profile.AccountId);
-                    playerDict.Add("d.p.id", damageInfo.Player.iPlayer.ProfileId);
-                }
-            }
-            catch (Exception e)
-            {
-                Logger.LogError(e);
-            }
-            damageInfo.Player = null;
-            Dictionary<string, string> weaponDict = new();
 
-            if (damageInfo.Weapon != null)
-            {
-                packet.Add("d.w.tpl", damageInfo.Weapon.TemplateId);
-                packet.Add("d.w.id", damageInfo.Weapon.Id);
-            }
-            damageInfo.Weapon = null;
+            //Dictionary<string, object> packet = new();
+            //damageInfo.HitCollider = null;
+            //damageInfo.HittedBallisticCollider = null;
+            //Dictionary<string, string> playerDict = new();
+            //try
+            //{
+            //    if (damageInfo.Player != null)
+            //    {
+            //        playerDict.Add("d.p.aid", damageInfo.Player.iPlayer.Profile.AccountId);
+            //        playerDict.Add("d.p.id", damageInfo.Player.iPlayer.ProfileId);
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Logger.LogError(e);
+            //}
+            //damageInfo.Player = null;
+            //Dictionary<string, string> weaponDict = new();
 
-            packet.Add("d", SerializeObject(damageInfo));
-            packet.Add("d.p", playerDict);
-            packet.Add("d.w", weaponDict);
-            packet.Add("bpt", bodyPartType.ToString());
-            packet.Add("ab", absorbed.ToString());
-            packet.Add("hs", headSegment.ToString());
-            packet.Add("m", "ApplyDamageInfo");
-            AkiBackendCommunicationCoopHelpers.PostLocalPlayerData(player, packet, true);
+            //if (damageInfo.Weapon != null)
+            //{
+            //    packet.Add("d.w.tpl", damageInfo.Weapon.TemplateId);
+            //    packet.Add("d.w.id", damageInfo.Weapon.Id);
+            //}
+            //damageInfo.Weapon = null;
+
+            //packet.Add("d", SerializeObject(damageInfo));
+            //packet.Add("d.p", playerDict);
+            //packet.Add("d.w", weaponDict);
+            //packet.Add("bpt", bodyPartType.ToString());
+            //packet.Add("ab", absorbed.ToString());
+            //packet.Add("hs", headSegment.ToString());
+            //packet.Add("m", "ApplyDamageInfo");
+            //AkiBackendCommunicationCoopHelpers.PostLocalPlayerData(player, packet, true);
 
 
             // ---------------------------- KILL ------------------------------
             //if (MatchmakerAcceptPatches.IsServer)  // should we do this on the server and send out to others only?
-            {
-                var bodyPartHealth = player.ActiveHealthController.GetBodyPartHealth(bodyPartType);
-                if (
-                    ((bodyPartType == EBodyPart.Head || bodyPartType == EBodyPart.Common || bodyPartType == EBodyPart.Chest) && bodyPartHealth.AtMinimum)
-                    || !player.ActiveHealthController.IsAlive
-                    || !player.PlayerHealthController.IsAlive
-                    )
-                {
-                    packet = new();
-                    packet.Add("accountId", player.Profile.AccountId);
-                    packet.Add("serverId", CoopGameComponent.GetServerId());
-                    packet.Add("t", DateTime.Now.Ticks.ToString("G"));
-                    packet.Add("dmt", damageInfo.DamageType.ToString());
-                    packet.Add("m", "Kill");
-                    AkiBackendCommunication.Instance.SendDataToPool(packet.ToJson());
-                }
-            }
+            //    {
+            //        var bodyPartHealth = player.ActiveHealthController.GetBodyPartHealth(bodyPartType);
+            //        if (
+            //            ((bodyPartType == EBodyPart.Head || bodyPartType == EBodyPart.Common || bodyPartType == EBodyPart.Chest) && bodyPartHealth.AtMinimum)
+            //            || !player.ActiveHealthController.IsAlive
+            //            || !player.PlayerHealthController.IsAlive
+            //            )
+            //        {
+            //            packet = new();
+            //            packet.Add("accountId", player.Profile.AccountId);
+            //            packet.Add("serverId", CoopGameComponent.GetServerId());
+            //            packet.Add("t", DateTime.Now.Ticks.ToString("G"));
+            //            packet.Add("dmt", damageInfo.DamageType.ToString());
+            //            packet.Add("m", "Kill");
+            //            AkiBackendCommunication.Instance.SendDataToPool(packet.ToJson());
+            //        }
+            //    }
         }
 
         public override void Replicated(EFT.Player player, Dictionary<string, object> dict)
@@ -162,25 +169,30 @@ namespace SIT.Core.Coop.Player
             if (HasProcessed(GetType(), player, dict))
                 return;
 
-            if (CallLocally.ContainsKey(player.Profile.AccountId))
+            if (CallLocally.Contains(player.ProfileId))
                 return;
 
-            try
+            if (player is CoopPlayer coopPlayer)
             {
-                Enum.TryParse<EBodyPart>(dict["bpt"].ToString(), out var bodyPartType);
-                Enum.TryParse<EHeadSegment>(dict["hs"].ToString(), out var headSegment);
-                var absorbed = float.Parse(dict["ab"].ToString());
-
-                var damageInfo = Player_ApplyShot_Patch.BuildDamageInfoFromPacket(dict);
-
-                CallLocally.Add(player.Profile.AccountId, true);
-                player.ApplyDamageInfo(damageInfo, bodyPartType, absorbed, headSegment);
-                player.ShotReactions(damageInfo, bodyPartType);
+                coopPlayer.ReceiveDamageFromServer(dict);
             }
-            catch (Exception e)
-            {
-                Logger.LogInfo(e);
-            }
+
+            //try
+            //{
+            //    Enum.TryParse<EBodyPart>(dict["bpt"].ToString(), out var bodyPartType);
+            //    Enum.TryParse<EHeadSegment>(dict["hs"].ToString(), out var headSegment);
+            //    var absorbed = float.Parse(dict["ab"].ToString());
+
+            //    var damageInfo = Player_ApplyShot_Patch.BuildDamageInfoFromPacket(dict);
+
+            //    CallLocally.Add(player.ProfileId);
+            //    player.ApplyDamageInfo(damageInfo, bodyPartType, absorbed, headSegment);
+            //    player.ShotReactions(damageInfo, bodyPartType);
+            //}
+            //catch (Exception e)
+            //{
+            //    Logger.LogInfo(e);
+            //}
 
             if (player.HealthController == null)
                 return;
