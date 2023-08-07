b372ad91360a4ab3110efa9c8ecd0adbb6ce0d0a
Paulov <pargsoft@hotmail.com>
2023-08-07 15:45:34 +0100

    Add Dvize NoBushESP

---
+++ Other/AI/Dvize/NoBushESP/NoBushESPPlugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIT.Core.Other.AI.Dvize.NoBushESP
+{
+    using System;
+    using System.Diagnostics;
+    using BepInEx;
+
+    namespace SIT.Core.Other.AI.Dvize.NoBushESP
+    {
+
+        [BepInPlugin("com.dvize.BushNoESP", "dvize.BushNoESP", "1.6.0")]
+        public class NoBushESPPlugin : BaseUnityPlugin
+        {
+            private void Awake()
+            {
+            }
+
+            public void Start() => new BushPatch().Enable();
+
+
+        }
+
+
+    }
+
+}
