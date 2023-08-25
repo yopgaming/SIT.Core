afc17c0a608ba6737ee6a83da0ef32bf930035a9
Paulov <pargsoft@hotmail.com>
2023-08-26 00:02:42 +0100

    Add Aki's Loot Cache Busting Patch - Thanks Chomp

---
+++ AkiSupport/Custom/LocationLootCacheBustingPatch.cs
@@ -0,0 +1,39 @@
+﻿using SIT.Tarkov.Core;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Aki.Custom.Patches
+{
+    /// <summary>
+    /// BaseLocalGame appears to cache a maps loot data and reuse it when the variantId from method_6 is the same, this patch randomises the id to make caching less common
+    /// </summary>
+    public class LocationLootCacheBustingPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            var desiredType = PatchConstants.EftTypes.Single(x => x.Name == "LocalGame").BaseType; // BaseLocalGame
+            var desiredMethod = desiredType.GetMethods(PatchConstants.PrivateFlags).Single(x => IsTargetMethod(x)); // method_6
+
+            Logger.LogDebug($"{this.GetType().Name} Type: {desiredType?.Name}");
+            Logger.LogDebug($"{this.GetType().Name} Method: {desiredMethod?.Name}");
+
+            return desiredMethod;
+        }
+
+        private static bool IsTargetMethod(MethodInfo mi)
+        {
+            var parameters = mi.GetParameters();
+            return parameters.Length == 3
+                && parameters[0].Name == "backendUrl"
+                && parameters[1].Name == "locationId"
+                && parameters[2].Name == "variantId";
+        }
+
+        [PatchPrefix]
+        private static bool PatchPrefix()
+        {
+            return false; // skip original
+        }
+    }
+}
